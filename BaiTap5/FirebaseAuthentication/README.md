292c8aadb16de90c21c7758063c4256b4c5ac88c
Hoàng Quý <2251120440@ut.edu.vn>
2025-03-28 21:35:43 +0700

    login with google use firebase authentication

---
+++ BaiTap5/FirebaseAuthentication/README.md
@@ -0,0 +1,27 @@
+# Ứng dụng Đăng nhập bằng Google cơ bản
+
+## Giới thiệu
+
+Ứng dụng cho phép người dùng đăng nhập bằng tài khoản Google thông qua Firebase Authentication. Sau khi đăng nhập thành công, ứng dụng sẽ hiển thị thông tin email của người dùng kèm theo thông báo thành công.
+
+## Tính năng
+
+- Đăng nhập bằng Google: Người dùng có thể đăng nhập bằng tài khoản Google một cách nhanh chóng và an toàn.
+
+- Xác thực Firebase: Tích hợp Firebase Authentication để xử lý đăng nhập và quản lý phiên người dùng.
+
+- Giao diện Compose UI:
+
+  - Nút đăng nhập đẹp mắt.
+
+  - Hiển thị thông báo thành công hoặc thất bại sau khi đăng nhập.
+
+## Công nghệ sử dụng
+
+- Kotlin: Ngôn ngữ lập trình chính của ứng dụng.
+
+- Jetpack Compose: Framework UI hiện đại của Android giúp xây dựng giao diện động.
+
+- Firebase Authentication: Dịch vụ xác thực để đăng nhập bằng tài khoản Google.
+
+- Google Sign-In API: Hỗ trợ đăng nhập thông qua tài khoản Google.
---
+++ BaiTap5/FirebaseAuthentication/app/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.firebaseauthentication"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.firebaseauthentication"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    // [START gradle_firebase_ui_auth]
+    implementation("com.firebaseui:firebase-ui-auth:9.0.0")
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.android.gms:play-services-auth:21.1.1")
+    implementation("com.google.firebase:firebase-auth-ktx:23.0.0")
+
+    // Add the dependency for the Firebase SDK for Google Analytics
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Also add the dependencies for the Credential Manager libraries and specify their versions
+    implementation("androidx.credentials:credentials:1.3.0")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
